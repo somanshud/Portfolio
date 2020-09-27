85ce2edb685cdef2b6e3b9826c56166ad80d9887
somanshu <somanshu.dhingra@gmail.com>
2020-09-27 20:31:52 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     path:'',
     component: LandingpageComponent,
     data:{
-      title:"Tushar Gulyani | Portfolio",
+      title:"Somanshu Dhingra | Portfolio",
       metaDescription: 'Landing page'
     }
   },                  
